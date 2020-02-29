2367ae41ce8b0f73b576f669a19c68e394bb4321
Sam Deane <deane_sam@yahoo.com>
2020-02-28 23:41:05 -0500

    Added getSummonerInfo by summoner name

---
+++ riotAPI/IDParsing.py
@@ -0,0 +1,6 @@
+import  json
+
+def getId(jsonFile):
+    return jsonFile.get("id")
+
+
--- riotAPI/riot_summoner.py
+++ riotAPI/riot_summoner.py
@@ -1,4 +1,5 @@
 import requests
+from riotAPI.IDParsing import *
 
 from riotAPI.auth import API_KEY
 from riotAPI.check_status import check_status_summoner
@@ -12,8 +13,14 @@ def getSummonerbyName(name):
 	check_status_summoner(response)
 	return response.json()
 
+def getSummonerInfo(name):
+	response = requests.get(URL + "/lol/league/v4/entries/by-summoner/" + getId(getSummonerbyName(name)) + Header)
+	check_status_summoner(response)
+	return response.json()
+
 def main():
 	print(getSummonerbyName("BigJonathan13"))
+	print(getSummonerInfo("elophantsoup"))
 	return
 
 if __name__ == '__main__':
